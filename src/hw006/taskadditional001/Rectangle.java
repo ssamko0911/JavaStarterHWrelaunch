54bebefad642255ba5ecb64d9cec62fd11afdb9c
samko.s <ssamko091184@gmail.com>
2022-05-04 18:16:13 +0300

    additional task001 done.

---
+++ src/hw006/taskadditional001/Rectangle.java
@@ -0,0 +1,30 @@
+package hw006.taskadditional001;
+
+/*
+Задание
+Используя IntelliJ IDEA, создайте класс Rectangle.
+Создайте две целочисленные переменные и задайте им некоторые значения. Применяя технику
+вложенных циклов, нарисуйте прямоугольник из звездочек. Используйте значения ранее созданных
+переменных для указания высоты и ширины прямоугольника.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        System.out.println("To draw rectangle, please, input height: ");
+        Scanner in = new Scanner(System.in);
+        int height = in.nextInt();
+        System.out.println("Input width: ");
+        int width = in.nextInt();
+        System.out.println("Rectangle:".toUpperCase(Locale.ROOT));
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
