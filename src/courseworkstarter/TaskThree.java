78aa00c0b02076288070244e58a2192f37db7634
samko.s <ssamko091184@gmail.com>
2022-05-13 12:33:28 +0300

    task003 done

---
+++ src/courseworkstarter/TaskThree.java
@@ -0,0 +1,28 @@
+package courseworkstarter;
+
+/*
+Задача 3.
+Создайте программу, которая выводит в консоль число имени, например, на вход Roman = R(18) +
+О(15) + M(13) + A(1) + N(14) = 61 на выход 61.
+
+ */
+
+import java.util.Scanner;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        System.out.println("Input your name to receive a sum of all digits from letters:");
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Sum is %d.", getSumOfAllDigitsFromLetters(in.next()));
+        in.close();
+    }
+
+    public static int getSumOfAllDigitsFromLetters(String someString) {
+        char[] charsFromString = someString.toCharArray();
+        int sum = 0;
+        for (char character : charsFromString) {
+            sum += character;
+        }
+        return sum;
+    }
+}
